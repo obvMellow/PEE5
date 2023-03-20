c5ede0ca1fd39d73c389b78d727554855082c163
Deniz Demir <darkenedMellow@protonmail.com>
2023-03-20 22:49:19 +0300

    Base structure of the bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use colored::Colorize;
+use serenity::async_trait;
+use serenity::client::bridge::gateway::ShardManagerError;
+use serenity::model::application::command::Command;
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
+use serenity::model::gateway::Ready;
+use serenity::model::id::GuildId;
+use serenity::prelude::*;
+
+type Result<T> = std::result::Result<T, ShardManagerError>;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            println!(
+                "{} {:#?}",
+                "Received Command Interaction:".green().bold(),
+                command
+            );
+
+            let result: Result<()> = match command.data.name.as_str() {
+                _ => Ok(()),
+            };
+
+            if let Err(why) = result {
+                eprintln!(
+                    "{} An error occured on a slash command: {:?}",
+                    "Error".red().bold(),
+                    why
+                );
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        // Create application commands here
+
+        println!(
+            "{} Connected as \"{}\"",
+            "Ready".green().bold(),
+            ready.user.tag()
+        );
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = std::env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let mut client = Client::builder(token, GatewayIntents::empty())
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("{} Client error: {:?}", "Error".red().bold(), why);
+    }
 }
