ddfd96646bcbb1e51925b827882f2e14bb60d6b2
Deniz Demir <darkenedMellow@protonmail.com>
2023-04-19 15:01:53 +0300

    Added rate limit to image generation

--- src/commands/imagine.rs
+++ src/commands/imagine.rs
@@ -1,6 +1,7 @@
 use std::{
-    fs::{self, File},
-    io::Write,
+    fs::{self, File, OpenOptions},
+    io::{Read, Write},
+    path::Path,
 };
 
 use crate::global_config::GlobalConfig;
@@ -14,8 +15,10 @@ use openai_gpt_rs::{
 };
 use rand::{distributions::Alphanumeric, Rng};
 use reqwest::Url;
-use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
 use serenity::model::prelude::interaction::InteractionResponseType;
+use serenity::model::prelude::interaction::{
+    application_command::ApplicationCommandInteraction, MessageFlags,
+};
 use serenity::model::prelude::{
     command::CommandOptionType, interaction::message_component::MessageComponentInteraction,
 };
@@ -50,6 +53,33 @@ pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> 
     let key = GlobalConfig::load("config.json").openai_key;
 
     let client = Client::new(&key);
+
+    let (timestamp, _new, mut file, contents) = should_continue(interaction.user.id.as_u64());
+
+    if !_new && timestamp - contents.replace("\0", "").parse::<i64>().unwrap() < 30 {
+        interaction
+            .create_interaction_response(&ctx.http, |response| {
+                response
+                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                    .interaction_response_data(|message| {
+                        message
+                            .embed(|embed| {
+                                embed
+                                    .title("You are going too fast!")
+                                    .description("You can use this command once every 30 seconds")
+                                    .colour(Colour::from_rgb(255, 0, 0))
+                            })
+                            .flags(MessageFlags::EPHEMERAL)
+                    })
+            })
+            .await
+            .unwrap();
+
+        return Ok(());
+    }
+
+    file.set_len(0).unwrap();
+    file.write_all(timestamp.to_string().as_bytes()).unwrap();
 
     interaction
         .create_interaction_response(&ctx.http, |response| {
@@ -156,6 +186,31 @@ pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> 
     Ok(())
 }
 
+fn should_continue(id: &u64) -> (i64, bool, File, String) {
+    fs::create_dir_all("imagine_requests/").unwrap();
+    let s = format!("imagine_requests/{}", id);
+    let file_path = Path::new(&s);
+
+    let timestamp = Timestamp::now().unix_timestamp();
+    let mut _new = false;
+
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(file_path)
+        .unwrap();
+
+    if file.metadata().unwrap().len() == 0 {
+        file.write_all(timestamp.to_string().as_bytes()).unwrap();
+        _new = true;
+    }
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    (timestamp, _new, file, contents)
+}
+
 pub async fn retry(ctx: &Context, component: &MessageComponentInteraction) -> Result<()> {
     let prompt = fs::read_to_string(format!(
         "tmp/{}:{}:{}:{}",
@@ -166,11 +221,9 @@ pub async fn retry(ctx: &Context, component: &MessageComponentInteraction) -> Re
     ))
     .unwrap();
 
-    let prompt = prompt.split("\n").collect::<Vec<&str>>()[0];
+    let url = prompt.split("\n").collect::<Vec<&str>>()[1];
 
-    let key = GlobalConfig::load("config.json").openai_key;
-
-    let client = Client::new(&key);
+    let (timestamp, _new, mut file, contents) = should_continue(component.user.id.as_u64());
 
     component
         .create_interaction_response(&ctx.http, |response| {
@@ -183,6 +236,68 @@ pub async fn retry(ctx: &Context, component: &MessageComponentInteraction) -> Re
         })
         .await
         .unwrap();
+
+    if !_new && timestamp - contents.replace("\0", "").parse::<i64>().unwrap() < 30 {
+        component
+            .edit_original_interaction_response(&ctx.http, |response| {
+                response.embed(|embed| {
+                    embed.title("Imagine");
+                    embed.description(
+                        RESPONSE_DESCRIPTION.to_owned()
+                            + "\n**Please wait 30 seconds before retrying**",
+                    );
+                    embed.color(Colour::from_rgb(255, 0, 0));
+                    embed.timestamp(&Timestamp::now());
+
+                    embed.image(&url);
+
+                    embed
+                });
+
+                response.components(|component| {
+                    component.create_action_row(|row| {
+                        row.create_button(|button| {
+                            button
+                                .label("Retry")
+                                .style(ButtonStyle::Primary)
+                                .custom_id("imagine_retry")
+                        })
+                        .create_button(|button| {
+                            button
+                                .custom_id("imagine_save")
+                                .style(ButtonStyle::Secondary)
+                                .label("Save")
+                        })
+                        .create_button(|button| {
+                            button
+                                .label("Support ❤️")
+                                .style(ButtonStyle::Link)
+                                .url("https://patreon.com/_mellow")
+                        })
+                        .create_button(|button| {
+                            button
+                                .label("Vote")
+                                .style(ButtonStyle::Link)
+                                .url("https://top.gg/bot/1087464844288069722/vote")
+                        })
+                    })
+                });
+
+                response
+            })
+            .await?;
+
+        return Ok(());
+    }
+
+    file.set_len(0).unwrap();
+    file.write_all(timestamp.to_string().as_bytes()).unwrap();
+
+    let prompt = prompt.split("\n").collect::<Vec<&str>>()[0];
+
+    let key = GlobalConfig::load("config.json").openai_key;
+
+    let client = Client::new(&key);
 
     component
         .edit_original_interaction_response(&ctx.http, |response| {
