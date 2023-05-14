d354333b1050520e82397650f36fe8375889b589
Deniz Demir <darkenedMellow@protonmail.com>
2023-05-15 01:11:43 +0300

    Updated xp plugin

--- src/plugins/xp.rs
+++ src/plugins/xp.rs
@@ -4,9 +4,9 @@ use serenity::model::prelude::Message;
 pub fn run(msg: &Message, config: &mut GuildConfig) {
     let users = config.get_users_mut();
 
-    let xp_gain: usize = 100;
+    let xp_gain = msg.content.split(" ").count() * 10;
 
     let xp = users.get_mut(&msg.author.id.0).unwrap();
 
-    *xp = (*xp + xp_gain).into();
+    *xp = xp_gain;
 }
