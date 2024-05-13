b95c6e5c694bc3f077ad9331bd1ec01a21280e46
mellow <darkenedmellow@protonmail.com>
2024-05-13 15:48:44 +0300

    Disabled chat plugin.

--- src/plugins/chat.rs
+++ src/plugins/chat.rs
@@ -17,75 +17,77 @@ const CHAT_COMMANDS: [&str; 3] = ["!end", "!rename", "!clear"];
 const CONTEXT_MSG_LIMIT: u64 = 50;
 
 pub async fn run(msg: &Message, ctx: &Context, config: &GuildConfig, guild_id: Option<GuildId>) {
-    let channel = msg.channel_id;
-
-    if CHAT_COMMANDS.iter().any(|v| msg.content.starts_with(*v)) && guild_id.is_some() {
-        match msg.content.split(' ').nth(0) {
-            Some("!end") => {
-                _end(&msg, channel, &ctx, guild_id, config).await;
-                return;
-            }
-            Some("!rename") => {
-                _rename(&msg, channel, &ctx).await;
-                return;
-            }
-            Some("!clear") => {
-                _clear(&msg, channel, &ctx).await;
-                return;
-            }
-            _ => (),
-        }
-    }
-
-    let typing = ctx.http.start_typing(msg.channel_id.0).unwrap();
-
-    let mut context_msg = channel
-        .messages(&ctx.http, |builder| builder.limit(CONTEXT_MSG_LIMIT))
-        .await
-        .unwrap();
-
-    context_msg.reverse();
-
-    let mut context = String::new();
-
-    for msg in context_msg {
-        context.push_str(
-            format!("Author: {}\nContent: {} \n\n", msg.author.name, msg.content).as_str(),
-        );
-    }
-    context.push_str(
-        "# Only include the content of your response, not the author, or \"Content:\" label.",
-    );
-
-    let context_msg = ChatMessage {
-        role: "assistant".to_string(),
-        content: context,
-    };
-
-    let user_msg = ChatMessage {
-        role: "user".to_string(),
-        content: msg.content.clone(),
-    };
-
-    let messages = vec![context_msg, user_msg];
-
-    let client = OpenAIClient::new(&GlobalConfig::load("config.json").openai_key);
-
-    let resp = client
-        .create_chat_completion(|args| {
-            args.max_tokens(1024)
-                .messages(messages)
-                .model(ChatModels::Gpt3_5Turbo)
-        })
-        .await;
-
-    let new_msg = match resp {
-        Ok(resp) => resp.get_content(0).unwrap(),
-        Err(error) => error.message,
-    };
-
-    msg.reply(&ctx.http, new_msg).await.unwrap();
-    typing.stop().unwrap();
+    msg.reply(&ctx.http, "AI features are temporarily disabled.").await.unwrap();
+    
+    // let channel = msg.channel_id;
+    //
+    // if CHAT_COMMANDS.iter().any(|v| msg.content.starts_with(*v)) && guild_id.is_some() {
+    //     match msg.content.split(' ').nth(0) {
+    //         Some("!end") => {
+    //             _end(&msg, channel, &ctx, guild_id, config).await;
+    //             return;
+    //         }
+    //         Some("!rename") => {
+    //             _rename(&msg, channel, &ctx).await;
+    //             return;
+    //         }
+    //         Some("!clear") => {
+    //             _clear(&msg, channel, &ctx).await;
+    //             return;
+    //         }
+    //         _ => (),
+    //     }
+    // }
+    //
+    // let typing = ctx.http.start_typing(msg.channel_id.0).unwrap();
+    //
+    // let mut context_msg = channel
+    //     .messages(&ctx.http, |builder| builder.limit(CONTEXT_MSG_LIMIT))
+    //     .await
+    //     .unwrap();
+    //
+    // context_msg.reverse();
+    //
+    // let mut context = String::new();
+    //
+    // for msg in context_msg {
+    //     context.push_str(
+    //         format!("Author: {}\nContent: {} \n\n", msg.author.name, msg.content).as_str(),
+    //     );
+    // }
+    // context.push_str(
+    //     "# Only include the content of your response, not the author, or \"Content:\" label.",
+    // );
+    //
+    // let context_msg = ChatMessage {
+    //     role: "assistant".to_string(),
+    //     content: context,
+    // };
+    //
+    // let user_msg = ChatMessage {
+    //     role: "user".to_string(),
+    //     content: msg.content.clone(),
+    // };
+    //
+    // let messages = vec![context_msg, user_msg];
+    //
+    // let client = OpenAIClient::new(&GlobalConfig::load("config.json").openai_key);
+    //
+    // let resp = client
+    //     .create_chat_completion(|args| {
+    //         args.max_tokens(1024)
+    //             .messages(messages)
+    //             .model(ChatModels::Gpt3_5Turbo)
+    //     })
+    //     .await;
+    //
+    // let new_msg = match resp {
+    //     Ok(resp) => resp.get_content(0).unwrap(),
+    //     Err(error) => error.message,
+    // };
+    //
+    // msg.reply(&ctx.http, new_msg).await.unwrap();
+    // typing.stop().unwrap();
 }
 
 async fn _end(
